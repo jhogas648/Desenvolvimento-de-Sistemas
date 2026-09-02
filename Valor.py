37f8aed8e2a72bfd560c12233d413eca09e4aa2f
jhogas648 <jhonatangabriel648@gmail.com>
2026-09-02 12:47:11 -0300

    Add files via upload

--- Valor.py
+++ Valor.py
@@ -1,5 +1,5 @@
-valor = input("Digite o valor da compra: ")
-if valor >= "300": 
+valor = float(input("Digite o valor da compra: "))
+if valor >= 200: 
     print("Compra com desconto")
 else:
     print("Compra sem desconto")
