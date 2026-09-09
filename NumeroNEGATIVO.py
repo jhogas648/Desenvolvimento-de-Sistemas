e68570c6271b9fc78c425bdab412093935ba9d04
jhogas648 <jhonatangabriel648@gmail.com>
2026-09-09 12:34:18 -0300

    Add files via upload

---
+++ NumeroNEGATIVO.py
@@ -0,0 +1,6 @@
+numero = -67
+
+if numero <= 0:
+    print("Seu saldo esta negativo")
+else:
+    print("Seu saldo não está negativo")
---
+++ desconto.py
@@ -0,0 +1,6 @@
+preço = float(input("Digite o preço: "))
+
+desconto = preço * 0.10
+
+
+print("desconto: ", desconto)
