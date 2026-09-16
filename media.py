eef1d377d3a85f25d0a4f4a187e45950f5d5e5a2
jhogas648 <jhonatangabriel648@gmail.com>
2026-09-16 12:43:40 -0300

    Add files via upload

--- media.py
+++ media.py
@@ -5,9 +5,12 @@ nota4 = int(input("digite a sua nota: "))
 
 media = (nota1+nota2+nota3+nota4)/4
 
+print("sua media é:",media)
+
 if media <= 3:
     print("reprovado")
 elif media <= 5:
     print("recuperaçao")
 else:
     print("aprovado")
+
