01a7958945fcae4d1e54a3d068c5154c90c4e9a5
jhogas648 <jhonatangabriel648@gmail.com>
2026-09-16 12:37:08 -0300

    Add files via upload

--- media.py
+++ media.py
@@ -1,11 +1,13 @@
-nota1 = float(input("Digite a nota do primeiro bimestre: "))
-nota2 = float(input("Digite a nota do segundo bimestre: "))
-nota3 = float(input("Digite a nota do terceiro bimestre: "))
-nota4 = float(input("Digite a nota do quarto bimestre: "))
+nota1 = int(input("digite a sua nota: "))
+nota2 = int(input("digite a sua nota: "))
+nota3 = int(input("digite a sua nota: "))
+nota4 = int(input("digite a sua nota: "))
 
-media =(nota1 + nota2 + nota3 + nota4) /4
+media = (nota1+nota2+nota3+nota4)/4
 
-if media >=5: 
-    print("Aluno Aprovado")
+if media <= 3:
+    print("reprovado")
+elif media <= 5:
+    print("recuperaçao")
 else:
-    print("Aluno Reprovado")
+    print("aprovado")
