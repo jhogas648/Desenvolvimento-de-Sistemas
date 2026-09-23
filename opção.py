f082877aa1536e6afed56172d77a9bc9caa184a9
jhogas648 <jhonatangabriel648@gmail.com>
2026-09-23 13:03:45 -0300

    Add files via upload

---
+++ opção.py
@@ -0,0 +1,19 @@
+opcao = int(input("digite uma das opções: "))
+
+if opcao == 1:
+    print("Tomar sorvete")
+    
+elif opcao == 2:
+    print("Comer lanche")
+    
+elif opcao == 3:
+    print("Comer pizza")
+    
+elif opcao == 4:
+    print("Comer esfiha")
+    
+elif opcao == 5:
+    print("comer salgado")
+    
+else:
+    print("opção invalida")
